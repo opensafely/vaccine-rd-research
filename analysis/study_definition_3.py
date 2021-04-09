5070f753d8c45343665ddf8fdfa2253f109574b6
Tom Palmer <remlapmot@hotmail.com>
2021-04-09 14:10:31 +0100

    comment out ethnicity_6_sus

--- analysis/study_definition_3.py
+++ analysis/study_definition_3.py
@@ -140,17 +140,15 @@ study = StudyDefinition(
         },
     ),
 
-    # ETHNICITY IN 6 CATEGORIES
-    ethnicity=patients.with_these_clinical_events(
-        ethnicity_codes,
-        returning="category",
-        find_last_match_in_period=True,
-        include_date_of_match=False,
-        return_expectations={
-            "category": {"ratios": {"1": 0.2, "2": 0.2, "3": 0.2, "4": 0.2, "5": 0.2}},
-            "incidence": 0.75,
-        },
-    ),
+    # # New ethnicity variable that takes data from SUS
+    # ethnicity_6_sus = patients.with_ethnicity_from_sus(
+    #     returning="group_6",  
+    #     use_most_frequent_code=True,
+    #     return_expectations={
+    #         "category": {"ratios": {"1": 0.2, "2": 0.2, "3": 0.2, "4": 0.2, "5": 0.2}},
+    #         "incidence": 0.4,
+    #         },
+    # ),
 
     ################################################
     ###### PRACTICE AND PATIENT ADDRESS VARIABLES ##
